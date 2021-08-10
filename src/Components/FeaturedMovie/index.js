d4766af4544ebbf90051be7795b52d5738274f77
Pedro Augusto <54971305+Pedroajs@users.noreply.github.com>
2021-08-09 23:12:52 -0300

    logica e visual do filme em destaque final

--- src/Components/FeaturedMovie/index.js
+++ src/Components/FeaturedMovie/index.js
@@ -2,6 +2,14 @@ import './style.css';
 
 
 export function FeaturedMovie(props){
+
+    const firstDate = new Date(props.item.first_air_date);
+    
+    let genres = [];
+    for(let i in props.item.genres){
+        genres.push(props.item.genres[i].name)
+    }
+
     return (
         <section className="featured"  style={{
             backgroundSize: 'cover',
@@ -10,7 +18,22 @@ export function FeaturedMovie(props){
         }}> 
             <div className="featured-vertical">
                 <div className="featured-horizontal">
+                    <div className="featured-name"> {props.item.original_name}  </div>
+                    <div className="featured-info">
+                        <div className="featured-points">{props.item.vote_average}</div>
+                        <div className="featured-release">{firstDate.getFullYear()}</div>
+                        <div className="featured-seasons">
+                            {props.item.number_of_seasons} temporada{props.item.number_of_seasons !== 1 ? 's': ''} 
+                        </div>
+                    </div>
+                    <div className="featured-description">{props.item.overview}</div>
 
+                    <div className="featured-buttons">
+                        <a href={`/watch/${props.item.id}`} className="featured-watchBtn">► Assistir </a>
+                        <a href={`/list/add${props.item.id}`} className="featured-listBtn">+ Minha lista </a>
+                    </div>
+
+                    <div className="featured-genres"> <strong>Gênero: {genres.join(', ')}</strong></div>
                 </div>
             </div>
             
